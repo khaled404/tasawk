de41f743e8cd24a0fe53f58a8e9c09071817579d
khaled404 <khaaleed.maahmmoud@gmail.com>
2019-09-25 13:16:25 +0200

    first commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,19 @@
+
+function time() {
+    let currentTime = new Date ();
+    function hours12(date) { return (date.getHours() + 24) % 12 || 12; }
+    let hour = hours12(currentTime);
+    let min = currentTime.getMinutes();
+    document.querySelector(".hour").textContent = hour;
+    document.querySelector(".min").textContent = min;
+}
+time()
+setInterval(() => {
+    time()
+},500);
+
+
+
 $(function(){
     
     $('.troger , .cross , .overlay').on('click',(e)=>{
@@ -9,5 +25,4 @@ $(function(){
         var scene = $('.scene').get(0);
         var parallaxInstance = new Parallax(scene);
     }
-
 });
